11d54438c4eaaa79573799ac1841edeb4a61a199
Ralph Soika <ralph.soika@imixs.com>
2018-02-10 18:04:58 +0100

    added new junit test for split events

--- imixs-archive-api/src/test/java/org/imixs/archive/core/TestSnapshotService.java
+++ imixs-archive-api/src/test/java/org/imixs/archive/core/TestSnapshotService.java
@@ -3,6 +3,7 @@ package org.imixs.archive.core;
 import static org.mockito.Mockito.when;
 
 import java.security.Principal;
+import java.util.List;
 import java.util.Properties;
 import java.util.logging.Logger;
 
@@ -38,8 +39,8 @@ public class TestSnapshotService {
 
 	@Spy
 	SnapshotService snapshotService;
-	
-	@Spy 
+
+	@Spy
 	PropertyService propertyService;
 
 	SessionContext ctx;
@@ -60,7 +61,7 @@ public class TestSnapshotService {
 		workflowMockEnvironment = new WorkflowMockEnvironment();
 		workflowMockEnvironment.setModelPath("/bpmn/TestSnapshotService.bpmn");
 		workflowMockEnvironment.setup();
-		
+
 		MockitoAnnotations.initMocks(this);
 
 		// mock session context
@@ -75,8 +76,8 @@ public class TestSnapshotService {
 
 		// mock property service
 		snapshotService.propertyService = propertyService;
-		Properties emptyProperties=new Properties();
-		when (propertyService.getProperties()).thenReturn(emptyProperties);
+		Properties emptyProperties = new Properties();
+		when(propertyService.getProperties()).thenReturn(emptyProperties);
 	}
 
 	/**
@@ -114,6 +115,100 @@ public class TestSnapshotService {
 		// load the snapshot workitem
 		ItemCollection snapshotworkitem = workflowMockEnvironment.getDatabase().get(snapshotID);
 		Assert.assertNotNull(snapshotworkitem);
+
+	}
+
+	/**
+	 * This test simulates a split event which results in a version of the origin
+	 * workitem. The file content must be migrated from the origin workitem also
+	 * into the version.
+	 * 
+	 * We simulate this here with a second call of snapshotService.onSave(). This is
+	 * a little bit ugly but I think the test integration is deep enough.
+	 * 
+	 * @throws ProcessingErrorException
+	 * @throws AccessDeniedException
+	 * @throws ModelException
+	 * 
+	 */
+	@Test
+	public void testOnSaveVersion()
+			throws AccessDeniedException, ProcessingErrorException, PluginException, ModelException {
+		// load test workitem
+		ItemCollection workitem = workflowMockEnvironment.getDatabase().get("W0000-00001");
+		workitem.replaceItemValue(WorkflowKernel.MODELVERSION, WorkflowMockEnvironment.DEFAULT_MODEL_VERSION);
+		workitem.replaceItemValue(WorkflowKernel.PROCESSID, 1000);
+		// trigger the split event
+		workitem.replaceItemValue(WorkflowKernel.ACTIVITYID, 10);
+
+		byte[] data = "This is a test".getBytes();
+		// we attache a file....
+		workitem.addFile(data, "test.txt", null);
+
+		DocumentEvent documentEvent = new DocumentEvent(workitem, DocumentEvent.ON_DOCUMENT_SAVE);
+		snapshotService.onSave(documentEvent);
+		workitem = workflowMockEnvironment.getWorkflowService().processWorkItem(workitem);
+
+		Assert.assertEquals("1.0.0", workitem.getItemValueString("$ModelVersion"));
+
+		// test the $snapshotID
+		Assert.assertTrue(workitem.hasItem(SnapshotService.SNAPSHOTID));
+		String snapshotID = workitem.getItemValueString(SnapshotService.SNAPSHOTID);
+		Assert.assertFalse(snapshotID.isEmpty());
+		logger.info("$snapshotid=" + snapshotID);
+		Assert.assertTrue(snapshotID.startsWith("W0000-00001"));
+
+		// load the snapshot workitem
+		ItemCollection snapshotworkitem = workflowMockEnvironment.getDatabase().get(snapshotID);
+		Assert.assertNotNull(snapshotworkitem);
+
+		// test the file content
+		List<Object> fileData = snapshotworkitem.getFile("test.txt");
+		byte[] content = (byte[]) fileData.get(1);
+		Assert.assertEquals("This is a test", new String(content));
+
+		/*
+		 * Now we trigger a second event to create a version be we remove the file
+		 * content before...
+		 */
+		workitem.replaceItemValue(WorkflowKernel.ACTIVITYID, 20);
+		documentEvent = new DocumentEvent(workitem, DocumentEvent.ON_DOCUMENT_SAVE);
+		snapshotService.onSave(documentEvent);
+		workitem = workflowMockEnvironment.getWorkflowService().processWorkItem(workitem);
+
+		// remove the file content from the origin....
+		workitem.removeFile("test.txt");
+		workflowMockEnvironment.getDocumentService().save(workitem);
+
+		/*
+		 * now lets check the version. The version should have the file content.
+		 */
+
+		// now lets check the version....
+		String versionID = workitem.getItemValueString("$uniqueIdVersions");
+
+		ItemCollection version = workflowMockEnvironment.getDatabase().get(versionID);
+		Assert.assertNotNull(version);
+
+		// simulate snaptshot cdi event
+		DocumentEvent documentEvent2 = new DocumentEvent(version, DocumentEvent.ON_DOCUMENT_SAVE);
+		snapshotService.onSave(documentEvent2);
+
+		// get the snapshot ID....
+		String versionSnapshot = version.getItemValueString("$snapshotid");
+		logger.info("Version $snapshotid=" + versionSnapshot);
+
+		// version snapshot id MUST not be equal
+		Assert.assertFalse(snapshotID.equals(versionSnapshot));
+
+		// we load the snapshot version and we expect again the fail content....
+		ItemCollection snapshotworkitemVersion = workflowMockEnvironment.getDatabase().get(versionSnapshot);
+		Assert.assertNotNull(snapshotworkitemVersion);
+
+		// test the file content
+		List<Object> fileDataVersion = snapshotworkitemVersion.getFile("test.txt");
+		byte[] contentVersion = (byte[]) fileDataVersion.get(1);
+		Assert.assertEquals("This is a test", new String(contentVersion));
 
 	}
 
