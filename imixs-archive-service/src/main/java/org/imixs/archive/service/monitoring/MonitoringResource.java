cceed926e94446d7d37c44a22d22042673781094
Ralph Soika <ralph.soika@imixs.com>
2018-12-05 19:05:57 +0100

    prometheus export format

--- imixs-archive-service/src/main/java/org/imixs/archive/service/monitoring/MonitoringResource.java
+++ imixs-archive-service/src/main/java/org/imixs/archive/service/monitoring/MonitoringResource.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -21,7 +23,17 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.StreamingOutput;
 
+
 /**
+ * This is a monitoring resource for  Prometheus
+ * 
+ * This class exports metries in prometheus text format.
+ * https://prometheus.io/docs/instrumenting/exposition_formats/
+ * 
+ * To avoid dependencies, we implement the prometheus exposition text format you
+ * ourself.
+ * 
+ * 
  * See:
  * http://www.adam-bien.com/roller/abien/entry/singleton_the_simplest_possible_jmx
  * http://www.adam-bien.com/roller/abien/entry/monitoring_java_ee_appservers_with
@@ -30,6 +42,10 @@ import javax.ws.rs.core.StreamingOutput;
  * 
  * - support receive events:
  * http://www.adam-bien.com/roller/abien/entry/java_ee_6_observer_with
+ * 
+ * 
+ * 
+ * 
  * 
  * 
  * @author rsoika
@@ -46,8 +62,11 @@ public class MonitoringResource {
 
 	private AtomicLong exceptionCount;
 
+	
+
 	@PostConstruct
 	public void registerInJMX() {
+		
 		this.exceptionCount = new AtomicLong();
 		try {
 
@@ -61,22 +80,35 @@ public class MonitoringResource {
 
 	/**
 	 * Prometheus format
+	 * 
+	 * Example:
+	 * 
+	 * http_requests_total{method="post",code="200"} 1027 1395066363000
+	 * http_requests_total{method="post",code="400"} 3 1395066363000
+	 * 
 	 */
 	@GET
-	@Produces({ MediaType.TEXT_PLAIN })
+	@Produces({ "text/plain; version=0.0.4" })
 	public Response getDiagnostics() {
+
+		
 		StreamingOutput stream = new StreamingOutput() {
 			@Override
 			public void write(OutputStream os) throws IOException, WebApplicationException {
 				Writer writer = new BufferedWriter(new OutputStreamWriter(os));
 
 				long time = System.currentTimeMillis();
+				Random rand = new Random();
+				int x = rand.nextInt((2000 - 1000) + 1) + 1000;
+				int y = rand.nextInt((500 - 10) + 1) + 10;
 
 				writer.write("# HELP http_requests_total The total number of HTTP requests." + "\n");
 				writer.write("# TYPE http_requests_total counter" + "\n");
-				writer.write("http_requests_total{method=\"post\",code=\"200\"} 1027 " + time + "\n");
-				writer.write("http_requests_total{method=\"post\",code=\"400\"}    3 " + time + "\n");
+				writer.write("http_requests_total{method=\"post\",code=\"200\"}  " + x + " " + time + "\n");
+				writer.write("http_requests_total{method=\"post\",code=\"400\"}  " + y + " " + time + "\n");
 
+				
+				// note: The last line must end with a line feed character. Empty lines are ignored.
 				writer.flush();
 			}
 		};
