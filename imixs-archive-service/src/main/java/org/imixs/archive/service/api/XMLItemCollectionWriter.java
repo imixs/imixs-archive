e68d8ccc5640ed0a1b7879af5915b56700db20be
Ralph Soika <ralph.soika@imixs.com>
2020-06-15 22:32:23 +0200

    refactoring

--- imixs-archive-service/src/main/java/org/imixs/archive/service/api/XMLItemCollectionWriter.java
+++ imixs-archive-service/src/main/java/org/imixs/archive/service/api/XMLItemCollectionWriter.java
@@ -25,7 +25,7 @@
  *  	Ralph Soika - Software Developer
  *******************************************************************************/
 
-package org.imixs.archive.service.rest;
+package org.imixs.archive.service.api;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
