5fae0f73f2671cbf387ac82838130ac4839edfee
Ralph Soika <ralph.soika@imixs.com>
2023-01-02 15:37:42 +0100

    doc

--- imixs-archive-service/src/main/java/org/imixs/archive/service/cassandra/ArchiveEvent.java
+++ imixs-archive-service/src/main/java/org/imixs/archive/service/cassandra/ArchiveEvent.java
@@ -3,16 +3,17 @@ package org.imixs.archive.service.cassandra;
 import org.imixs.workflow.ItemCollection;
 
 /**
- * The ArchiveEvent provides a CDI observer pattern. The ArchiveEvent is fired
- * by the DocumentService EJB. An event Observer can react on a save or load
- * event.
+ * The Imixs Archive Service provides Metric events for an external monitoring.
+ * The ArchiveEvent is fired by the DataService EJB each time a snapshot is
+ * stored in the cassandra cluster. An event Observer can react on a save or
+ * load event.
  * 
  * 
- * The DocumentEvent defines the following event types:
+ * The ArchiveEvent defines the following event types:
  * <ul>
- * <li>ON_DOCUMENT_SAVE - send immediately before a document will be saved
- * <li>ON_DOCUMENT_LOAD - send immediately after a document was loaded
- * <li>ON_DOCUMENT_DELETE - send immediately before a document will be deleted
+ * <li>ON_ARCHIVE - send immediately before a document will be saved
+ * <li>ON_RESTORE - send immediately after a document was restored
+ * <li>ON_DELETE - send immediately before a document will be deleted
  * </ul>
  * 
  * @author Ralph Soika
@@ -21,24 +22,24 @@ import org.imixs.workflow.ItemCollection;
  */
 public class ArchiveEvent {
 
-    public static final int ON_ARCHIVE = 1;
-    public static final int ON_RESTORE = 2;
-    public static final int ON_DELETE = 3;
+	public static final int ON_ARCHIVE = 1;
+	public static final int ON_RESTORE = 2;
+	public static final int ON_DELETE = 3;
 
-    private int eventType;
-    private ItemCollection document;
+	private int eventType;
+	private ItemCollection document;
 
-    public ArchiveEvent(ItemCollection document, int eventType) {
-        this.eventType = eventType;
-        this.document = document;
-    }
+	public ArchiveEvent(ItemCollection document, int eventType) {
+		this.eventType = eventType;
+		this.document = document;
+	}
 
-    public int getEventType() {
-        return eventType;
-    }
+	public int getEventType() {
+		return eventType;
+	}
 
-    public ItemCollection getDocument() {
-        return document;
-    }
+	public ItemCollection getDocument() {
+		return document;
+	}
 
 }
